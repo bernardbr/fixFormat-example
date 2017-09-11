96ae34babf2f7a4ea59fb102d73c94c2a1717c36
Bernardo Esbérard <bernardo.dsn.erp@alterdata.com.br>
2017-09-11 15:59:55 -0300

    Sample for https://github.com/Leopotam/vscode-csharpfixformat/issues/21

---
+++ src/app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using example;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.GetSelectScript());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/example/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace example
+{
+    public class Class1
+    {
+        /// <summary>
+        /// My summary.
+        /// </summary>
+        /// <returns>The SQL Select.</returns>
+        public static string GetSelectScript()
+        {
+            var tableName = "Table";
+            var fields = new List<string>(3);
+            fields.Add("first");
+            fields.Add("second");
+            fields.Add("last");
+
+            var keyName = "key_name";
+            var ids = new List<string>(1);
+            ids.Add($"{keyName} = @{keyName}");
+
+            return $@"
+                SELECT {string.Join(", ", fields)}
+                FROM {tableName}
+                WHERE {string.Join(" AND ", ids)}
+            ";
+        }
+    }
+}
